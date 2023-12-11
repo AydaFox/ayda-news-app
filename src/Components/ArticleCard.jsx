16736ac2f6e9159a880f509a7e2f9bf8788bba38
AydaFox <burrhe.y02@gmail.com>
2023-12-11 14:58:48 +0000

    adds functionality to display a list of the first page of all articles

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,8 +1,16 @@
-const ArticleCard = () => {
+const ArticleCard = ({ article }) => {
     return (
-        <div className="article-card">
-            Article Card
-        </div>
+        <li className="article-card">
+            <p className="article-title">{ article.title }</p>
+            <img src={ article.article_img_url }></img>
+            <p className="article-author">{ article.author }</p>
+            <p className="article-topic">{ article.topic }</p>
+            <div className="article-votes">
+                <img src="./src/assets/arrow-up-black.png"></img>
+                <span className="vote-number">{ article.votes }</span>
+                <img src="./src/assets/arrow-down-black.png"></img>
+            </div>
+        </li>
     );
 }
 
--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -1,10 +1,25 @@
 import ArticleCard from "./ArticleCard";
+import { useEffect, useState } from "react";
+import { getArticles } from "../utils/axios";
 
 const Articles = () => {
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        getArticles().then((articles) => {
+            setArticles(articles);
+        });
+    }, []);
+
     return (
         <section className="articles">
-            Articles
-            <ArticleCard />
+            <ul className="articles-list">
+                {
+                    articles.map((article) => {
+                        return (<ArticleCard key={ article.article_id } article={ article }/>);
+                    })
+                }
+            </ul>
         </section>
     );
 }
